4ab67d255f8c4ec93f6e805fa0a101072f17cafc
Sourish Kanna <sourishkanna001@gmail.com>
2025-11-02 00:36:23 +0530

    feat: Integrate Firebase Performance Monitoring for API calls
    - Adds the `firebase_performance` package to monitor the performance of network requests.
    - Implements custom traces `post_request` and `get_request` within the `ApiService` to measure the duration of POST and GET API calls.
    - Includes `devtools_options.yaml` for Dart & Flutter DevTools extension configuration.

--- lib/Providers/api_caller.dart
+++ lib/Providers/api_caller.dart
@@ -1,12 +1,14 @@
 import 'package:dio/dio.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-
+import 'package:firebase_performance/firebase_performance.dart';
 
 class ApiService {
   final Dio _dio = Dio();
   final String baseUrl = const String.fromEnvironment('API_LINK');
 
   Future<Response> post(String path, {Map<String,dynamic>? data}) async {
+    final trace = FirebasePerformance.instance.newTrace('post_request');
+    await trace.start();
     final user = FirebaseAuth.instance.currentUser;
     if (user == null) {
       throw Exception('No user logged in');
@@ -25,11 +27,14 @@ class ApiService {
       ),
       data: data
     );
+    await trace.stop();
 
     return response;
   }
 
   Future<Response> get(String path, {Map<String,dynamic>? queryParameters}) async {
+    final trace = FirebasePerformance.instance.newTrace('get_request');
+    await trace.start();
     final user = FirebaseAuth.instance.currentUser;
     if (user == null) {
       throw Exception('No user logged in');
@@ -48,6 +53,7 @@ class ApiService {
       ),
       queryParameters: queryParameters,
     );
+    await trace.stop();
     return response;
   }
 }
